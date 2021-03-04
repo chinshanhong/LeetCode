a86ea0d5adbfe091f8ed890dd3daf606ab868dcb
Chin Shan Hong <72982917+chinshanhong@users.noreply.github.com>
2021-03-04 16:27:22 +0800

    Create Day04 Intersection of Two Linked Lists.java

---
+++ March LeetCoding Challenge(2021)/Day04 Intersection of Two Linked Lists.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+//Find the intersection of two ListNodes using Hashing
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        HashSet<ListNode>  set = new HashSet<ListNode>();
+        while(headA != null) {
+            set.add(headA);
+            headA = headA.next;
+        }
+        while(headB != null){
+            if(set.contains(headB)) return headB;
+            headB = headB.next;
+        }
+        return null;
+    }
+}
