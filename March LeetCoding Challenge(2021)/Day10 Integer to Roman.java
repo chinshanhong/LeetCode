d32768ac4400af5e14adac8bb6e2648ae0e35a9a
Chin Shan Hong <72982917+chinshanhong@users.noreply.github.com>
2021-03-11 11:37:15 +0800

    Create Day10 Integer to Roman.java

---
+++ March LeetCoding Challenge(2021)/Day10 Integer to Roman.java
@@ -0,0 +1,37 @@
+class Solution{
+     private Numeral[] numerals = {
+             new Numeral("M", 1000),
+             new Numeral("CM", 900),
+             new Numeral("D", 500),
+             new Numeral("CD", 400),
+             new Numeral("C", 100),
+             new Numeral("XC", 90),
+             new Numeral("L", 50),
+             new Numeral("XL", 40),
+             new Numeral("X", 10),
+             new Numeral("IX", 9),
+             new Numeral("V", 5),
+             new Numeral("IV", 4),
+             new Numeral("I", 1),
+        };
+        
+        public String intToRoman(int num) {
+            String result = "";
+            for(Numeral numeral : numerals){
+                int numberOfSymbols = num / numeral.value;
+                if(numberOfSymbols != 0) result += numeral.symbol.repeat(numberOfSymbols);
+                num %= numeral.value;
+            }
+            return result;
+        }
+        class Numeral {
+            public String symbol;
+            public int value;
+            public Numeral(String symbol, int value){
+                this.symbol = symbol;
+                this.value = value;
+            }
+        }
+}
+
+        
