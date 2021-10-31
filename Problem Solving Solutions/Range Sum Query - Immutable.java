126de80a796f1f2916858bc1c87a13dafa07a1a4
Chin Shan Hong <72982917+chinshanhong@users.noreply.github.com>
2021-10-31 17:26:15 +0800

    Create Range Sum Query - Immutable.java

---
+++ Problem Solving Solutions/Range Sum Query - Immutable.java
@@ -0,0 +1,20 @@
+class NumArray {
+    private int[] nums;
+    public NumArray(int[] nums) {
+        this.nums = nums;
+    }
+    
+    public int sumRange(int left, int right) {
+        int res = 0;
+        while(left <= right){
+            res += nums[left];
+            left++;
+        }
+        return res;
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
