bc1e9650a1e802e4d1bd90ad3f55bc3966d2f003
Chin Shan Hong <72982917+chinshanhong@users.noreply.github.com>
2022-01-16 11:29:11 +0800

    Update Maximize Distance to Closest Person.java

--- Problem Solving Solutions/Maximize Distance to Closest Person.java
+++ Problem Solving Solutions/Maximize Distance to Closest Person.java
@@ -10,15 +10,21 @@ class Solution {
         int maxDistance = 0;
         
         for(int i = 0; i < indexList.size() - 1; i++){
-            maxDistance = Math.max(maxDistance, ((indexList.get(i + 1) - indexList.get(i)) / 2)) ;
+            if(maxDistance < (indexList.get(i + 1) - indexList.get(i)) / 2){
+                maxDistance = (indexList.get(i + 1) - indexList.get(i)) / 2;
+            }
         }
         
         if(indexList.get(0) != 0){
-            maxDistance = Math.max(maxDistance, indexList.get(0) - 0);
+            if(maxDistance <  indexList.get(0) - 0){
+                maxDistance =  indexList.get(0) - 0;
+            }
         }
         
         if(indexList.get(indexList.size() - 1) != seats.length - 1){
-            maxDistance = Math.max(maxDistance, (seats.length - 1) - indexList.get(indexList.size() - 1));
+            if(maxDistance <  (seats.length - 1) - indexList.get(indexList.size() - 1)){
+                maxDistance =  (seats.length - 1) - indexList.get(indexList.size() - 1);
+            }
         }
         
         return maxDistance;
