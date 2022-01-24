9fbf87bb6616279bc1a02dd4637ddb43007ca379
Chin Shan Hong <72982917+chinshanhong@users.noreply.github.com>
2022-01-24 08:21:15 +0800

    Create Detect Capital.java

---
+++ Problem Solving Solutions/Detect Capital.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        if(word.length() == 1){
+            return true;
+        }
+        
+        if(Character.isUpperCase(word.charAt(0)) 
+           && Character.isLowerCase(word.charAt(1))){
+            for(int i = 2; i < word.length(); i++){
+                if(Character.isUpperCase(word.charAt(i))){
+                    return false;
+                }
+            }
+        }
+        else if(Character.isLowerCase(word.charAt(0))){
+            for(int i = 1; i < word.length(); i++){
+                if(Character.isUpperCase(word.charAt(i))){
+                    return false;
+                }
+            }
+        }
+        else{
+            for(int i = 2; i < word.length(); i++){
+                if(Character.isLowerCase(word.charAt(i))){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
